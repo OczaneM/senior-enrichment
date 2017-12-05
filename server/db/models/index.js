afbf2d77cc3a37fda6361b03d56b0593de950bc9
Oczane <oczane.rivera@gmail.com>
2017-12-05 04:57:16 -0500

    added models & associations to model/index

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -2,11 +2,17 @@
 
 const db = require('../index');
 
-// Require all the models
-	// Running each model (i.e. table) module (i.e. file) registers each model into our sequelize db
-	// This works if we all use the same Sequelize instance (instantiated in and exported from `/db/index.js`)
-	// Exporting all models from here seems like a good idea!
+//All Models
+const Student = require('./models/Student')
+const Campus = require('./model/Campus')
 
-// This is also probably a good place for you to set up your associations
+//Associations
+Student.belongsTo(Campus)
+Campus.hasMany(Student)
 
-module.exports = db
+// Exporting all models
+module.exports = {
+	db,
+	Student,
+	Campus
+}
