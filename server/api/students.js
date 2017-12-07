8ac8aac770d0a6dec2168011556ba16d21a15bf4
Oczane <oczane.rivera@gmail.com>
2017-12-07 10:29:43 -0500

    defined the studen.js routes

--- server/api/students.js
+++ server/api/students.js
@@ -1,14 +1,18 @@
+import { create } from 'domain';
+
 'use strict'
 const router = require('express').Router()
 const {Campus, Student} = require('../db/models')
 module.exports = router
 
+//Get all students
 router.get('/', (req, res, next) => {
   Student.findAll({include: [Campus]})
     .then( foundStudent => res.json(foundStudent))
     .catch(next)
 })
 
+//Create a student
 router.post('/', (req, res, next) => {
   Student.create(req.body)
     .then( createdStudent => {
@@ -16,4 +20,28 @@ router.post('/', (req, res, next) => {
         where: {id: createdStudent.id},
         include: [Campus]})
     })
+    .then( createdStudent => res.json(createdStudent))
+    .catch(next)
+})
+
+//Updated a specific student
+router.put('/:id', (req, res, next) => {
+  Student.update(req.body, {
+    where: {id: req.params.id},
+    returning: true
+  })
+  .then( updatedStudentArray => {
+    return Student.findOne({
+      where: {id: updatedStudentArray[1][0].id},
+      include: [Campus] })
+      .then( updatedStudent => res.json(updatedStudent))
+      .catch(next)
+  })
+})
+
+//Delete a specific student
+router.delete('/:id', (req, res, next) => {
+  Student.destroy({where: {id: req.body.id}})
+  .then( () => res.statusMessage(204))
+  .catch(next)
 })
