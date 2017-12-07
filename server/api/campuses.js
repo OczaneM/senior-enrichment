7c738cc8d0e2fccb86b64652c4b03725e341824b
Oczane <oczane.rivera@gmail.com>
2017-12-07 10:57:43 -0500

    defined campus routes

---
+++ server/api/campuses.js
@@ -0,0 +1,36 @@
+'use strict'
+const router = require('express').Router()
+const {Campus, Student} = require('../db/models')
+module.exports = router
+
+//Get all students
+router.get('/', (req, res, next) => {
+  Campus.findAll()
+    .then( foundCampus => res.json(foundCampus))
+    .catch(next)
+})
+
+//Create a Campus
+router.post('/', (req, res, next) => {
+  Campus.create(req.body)
+    .then( createdCampus => Campus.findOne({where: {id: createdCampus.id}}))
+    .then( createdCampus => res.json(createdCampus))
+    .catch(next)
+})
+
+//Updated a specific Campus
+router.put('/:id', (req, res, next) => {
+  Campus.update(req.body, {
+    where: {id: req.params.id},
+    returning: true
+  })
+  .then( updatedCampusArray => res.json(updatedCampusArray[1][0]))
+  .catch(next)
+})
+
+//Delete a specific Campus
+router.delete('/:id', (req, res, next) => {
+  Campus.destroy({where: {id: req.body.id}})
+  .then( () => res.statusMessage(204))
+  .catch(next)
+})
