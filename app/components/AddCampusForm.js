09f9f11a9fb3072a5e6e18d8d6b26de65b037b92
Oczane <oczane.rivera@gmail.com>
2017-12-09 07:21:20 -0500

    modified AddCampusForm.js

--- app/components/AddCampusForm.js
+++ app/components/AddCampusForm.js
@@ -13,12 +13,12 @@ export default class AddCampusForm extends Component {
     this.handleSubmit = this.handleSubmit.bind(this)
   }
 
-  componentDidMount () {
-    this.unsubscribe = store.subscribe( () => this.setState(store.getState()))
-  }
-  componentWillUnmount () {
-    this.unsubscribe()
-  }
+  // componentDidMount () {
+  //   this.unsubscribe = store.subscribe( () => this.setState(store.getState()))
+  // }
+  // componentWillUnmount () {
+  //   this.unsubscribe()
+  // }
 
   handleChange (event) {
     this.setState({ [event.target.name]: event.target.name.value})
@@ -48,10 +48,13 @@ export default class AddCampusForm extends Component {
           <label>ADD A CAMPUS</label>
           <form id="new-campus-form" onSubmit={this.handleSubmit} >
             <label>Campus name </label>
+
             <input name="campusName" type="text" placeholder="New Campus Name" value={this.state.campusName} onChange={this.handleChange} /><br />
+
             <label>Campus Image </label>
-            <input name="campusImage" type="url" placeholder="imgUrl" onChange={this.handleChange} /><br />
-            <textarea name="campusDesc" rows="4" cols="35" placeholder="Brief campus decription" value={this.campusDesc} onChange={this.handleChange} /><br />
+            <input name="campusImage" type="url" placeholder="imgUrl" value={this.state.campusImage} onChange={this.handleChange} /><br />
+
+            <textarea name="campusDesc" rows="4" cols="35" placeholder="Brief campus decription" value={this.state.campusDesc} onChange={this.handleChange} /><br />
             <button type="submit" className="btn submit">Submit</button>
           </form>
         </center>
